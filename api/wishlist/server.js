66e0bf5b648396885a45c3fbeb0ebe8ab950f3b7
Glenn de Haan <glenn@dehaan.cloud>
2021-11-08 13:50:00 +0100

    Added the get wishlist route

--- api/wishlist/server.js
+++ api/wishlist/server.js
@@ -1,4 +1,9 @@
 /**
+ * Import vendor modules
+ */
+const fetch = require('node-fetch');
+
+/**
  * Check if we are using the dev version
  */
 const dev = process.env.NODE_ENV !== 'production';
@@ -21,8 +26,29 @@ const api = new Api('wishlist', 4005, version);
 /**
  * Setup GET Wishlist endpoint
  */
-api.get('/', (req, res) => {
-    res.json(api.response());
+api.get('/:user', (req, res) => {
+    const user = req.params.user || false;
+
+    fetch(dev ? `http://localhost:5000/wishlist/${user}` : `http://storage:5000/wishlist/${user}`, {
+        method: 'get',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            if(data.status.success) {
+                res.json(api.response(200, data.data, data.status));
+            } else {
+                res.status(404).json(api.response(404, {
+                    error: data.data
+                }, data.status));
+            }
+        }).catch((e) => {
+            res.status(500).json(api.response(500, {
+                error: e.message || false
+            }));
+        });
 });
 
 /**
