501360df0df63cbf81169279fb540d8647c8aa37
Glenn de Haan <glenn@dehaan.cloud>
2021-11-17 16:12:15 +0100

    Added the cart add, update and remove endpoints

--- api/cart/server.js
+++ api/cart/server.js
@@ -52,6 +52,121 @@ api.get('/:user', (req, res) => {
 });
 
 /**
+ * Setup POST Cart add endpoint
+ */
+api.post('/:user/add', (req, res) => {
+    const user = req.params.user || false;
+    const id = req.body.id || false;
+    const options = req.body.options || false;
+
+    if(!id || !options) {
+        res.json(api.response(412, {}));
+        return;
+    }
+
+    fetch(dev ? `http://localhost:5000/cart/${user}/add` : `http://storage:5000/cart/${user}/add`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: id.toString(),
+            options
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            if(data.status.success) {
+                res.json(api.response(200, data.data, data.status));
+            } else {
+                res.status(404).json(api.response(404, {
+                    error: data.data
+                }, data.status));
+            }
+        }).catch((e) => {
+            res.status(500).json(api.response(500, {
+                error: e.message || false
+            }));
+        });
+});
+
+/**
+ * Setup POST Cart update endpoint
+ */
+api.post('/:user/update', (req, res) => {
+    const user = req.params.user || false;
+    const id = req.body.id || false;
+    const options = req.body.options || false;
+
+    if(!id || !options) {
+        res.json(api.response(412, {}));
+        return;
+    }
+
+    fetch(dev ? `http://localhost:5000/cart/${user}/update` : `http://storage:5000/cart/${user}/update`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: id.toString(),
+            options
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            if(data.status.success) {
+                res.json(api.response(200, data.data, data.status));
+            } else {
+                res.status(404).json(api.response(404, {
+                    error: data.data
+                }, data.status));
+            }
+        }).catch((e) => {
+            res.status(500).json(api.response(500, {
+                error: e.message || false
+            }));
+        });
+});
+
+/**
+ * Setup POST Cart remove endpoint
+ */
+api.post('/:user/remove', (req, res) => {
+    const user = req.params.user || false;
+    const id = req.body.id || false;
+
+    if(!id) {
+        res.json(api.response(412, {}));
+        return;
+    }
+
+    fetch(dev ? `http://localhost:5000/cart/${user}/remove` : `http://storage:5000/cart/${user}/remove`, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: id.toString()
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            if(data.status.success) {
+                res.json(api.response(200, data.data, data.status));
+            } else {
+                res.status(404).json(api.response(404, {
+                    error: data.data
+                }, data.status));
+            }
+        }).catch((e) => {
+            res.status(500).json(api.response(500, {
+                error: e.message || false
+            }));
+        });
+});
+
+/**
  * Start the server
  */
 api.init();
--- storage/modules/cart.js
+++ storage/modules/cart.js
@@ -28,4 +28,161 @@ module.exports = function cart(api, redis) {
             res.json(api.response(200, JSON.parse(cartRes)));
         });
     });
+
+    /**
+     * Setup cart add endpoint
+     */
+    api.post('/cart/:user/add', (req, res) => {
+        const user = req.params.user;
+        const id = req.body.id;
+        const options = req.body.options;
+
+        redis.get(`cart_${user}`, (err, cartRes) => {
+            if(err) {
+                res.status(500).json(500, api.response({
+                    error: err
+                }));
+
+                return;
+            }
+
+            if(cartRes === null) {
+                res.status(404).json(api.response(404, {}));
+                return;
+            }
+
+            const cart = JSON.parse(cartRes);
+            const filter = cart.filter((item) => {
+                return item.id === id;
+            });
+
+            if(filter.length > 0) {
+                res.status(409).json(api.response(409, {}));
+                return;
+            }
+
+            cart.push({
+                id,
+                options
+            });
+
+            redis.set(`cart_${user}`, JSON.stringify(cart), (err) => {
+                if(err) {
+                    res.status(500).json(api.response(500, {
+                        error: err
+                    }));
+
+                    return;
+                }
+
+                res.json(api.response(200, cart));
+            });
+        });
+    });
+
+    /**
+     * Setup cart update endpoint
+     */
+    api.post('/cart/:user/update', (req, res) => {
+        const user = req.params.user;
+        const id = req.body.id;
+        const options = req.body.options;
+
+        redis.get(`cart_${user}`, (err, cartRes) => {
+            if(err) {
+                res.status(500).json(500, api.response({
+                    error: err
+                }));
+
+                return;
+            }
+
+            if(cartRes === null) {
+                res.status(404).json(api.response(404, {}));
+                return;
+            }
+
+            const cart = JSON.parse(cartRes);
+            const filter = cart.filter((item) => {
+                return item.id === id;
+            });
+
+            if(filter < 1) {
+                res.status(404).json(api.response(404, {}));
+                return;
+            }
+
+            const newCart = cart.map((item) => {
+                if(item.id === id) {
+                    return {
+                        id,
+                        options
+                    }
+                }
+
+                return item;
+            });
+
+            redis.set(`cart_${user}`, JSON.stringify(newCart), (err) => {
+                if(err) {
+                    res.status(500).json(api.response(500, {
+                        error: err
+                    }));
+
+                    return;
+                }
+
+                res.json(api.response(200, newCart));
+            });
+        });
+    });
+
+    /**
+     * Setup cart remove endpoint
+     */
+    api.post('/cart/:user/remove', (req, res) => {
+        const user = req.params.user;
+        const id = req.body.id;
+
+        redis.get(`cart_${user}`, (err, cartRes) => {
+            if(err) {
+                res.status(500).json(500, api.response({
+                    error: err
+                }));
+
+                return;
+            }
+
+            if(cartRes === null) {
+                res.status(404).json(api.response(404, {}));
+                return;
+            }
+
+            const cart = JSON.parse(cartRes);
+            const filter = cart.filter((item) => {
+                return item.id === id;
+            });
+
+            if(filter < 1) {
+                res.status(404).json(api.response(404, {}));
+                return;
+            }
+
+            const newCart = cart.filter((item) => {
+                return item.id !== id;
+            });
+
+            redis.set(`cart_${user}`, JSON.stringify(newCart), (err) => {
+                if(err) {
+                    res.status(500).json(api.response(500, {
+                        error: err
+                    }));
+
+                    return;
+                }
+
+                res.json(api.response(200, newCart));
+            });
+        });
+    });
 };
